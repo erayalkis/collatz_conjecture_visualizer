d9a5ba12bc22d458be76d5a69ab1888837d01481
Lily <lolpo12345@outlook.com>
2022-04-13 22:26:20 +0300

    Create main method

--- main.rb
+++ main.rb
@@ -14,3 +14,28 @@ class Node
   end
 
 end
+
+
+
+def main(n)
+  head = Node.new(n)
+
+  curr = n
+  until curr == 1
+    puts curr == 1
+
+    if curr % 2 == 0
+      curr = (curr * 3) + 1
+      head.next = Node.new(curr)
+    else
+      curr = curr / 2
+      head.next = Node.new(curr)
+    end
+  end
+
+  head
+end
+
+result = main(21)
+
+result.iterate_rest
