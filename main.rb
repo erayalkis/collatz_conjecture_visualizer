a4d6438b44e70230efbb6c1f1370b68d14bd5e06
Lily <lolpo12345@outlook.com>
2022-04-14 00:16:37 +0300

    Change method structure

--- main.rb
+++ main.rb
@@ -31,26 +31,21 @@ end
 def main(n)
   return puts "n can't be 0 or lower (n is #{n})" if n <= 0
 
-  head = Node.new(n)
-  steps = 0
+  data = {}
+  step = 0
 
-  curr_node = head
   curr = n
   until curr == 1
     if curr % 2 != 0
       curr = (curr * 3) + 1
-      curr_node.next = Node.new(curr)
-      curr_node = curr_node.next
       steps += 1
     else
       curr = curr / 2
-      curr_node.next = Node.new(curr)
-      curr_node = curr_node.next
       steps += 1
     end
   end
 
-  puts "Took #{steps} steps to reach loop"
+  puts "Took #{step} steps to reach loop"
   head
 end
 
