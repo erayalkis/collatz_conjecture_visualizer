19920018aa8395ca26fc62a68c00d8e1bcf3d28f
Lily <lolpo12345@outlook.com>
2022-04-13 22:41:10 +0300

    Add logging for steps taken until loop

--- main.rb
+++ main.rb
@@ -22,25 +22,26 @@ end
 
 def main(n)
   head = Node.new(n)
+  steps = 0
 
   curr_node = head
   curr = n
   until curr == 1
-
     if curr % 2 != 0
       curr = (curr * 3) + 1
       curr_node.next = Node.new(curr)
       curr_node = curr_node.next
+      steps += 1
     else
       curr = curr / 2
       curr_node.next = Node.new(curr)
       curr_node = curr_node.next
+      steps += 1
     end
   end
 
+  puts "Took #{steps} steps to reach loop"
   head
 end
 
-result = main(7)
-
-result.iterate_rest
+result = main(26)
