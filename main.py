0f18a0e8dec543782f2fe37ec1df26842542adf0
Lily <lolpo12345@outlook.com>
2022-04-14 03:59:07 +0300

    Enable directory checking

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 import time
-
+from os import path, mkdir
 
 def run_math(n: int):
   data = []
@@ -26,6 +26,12 @@ def run_math(n: int):
   
   return data
 
+def check_dirs():
+  dirs = ["./out_image", "./out_html"]
+
+  for directory in dirs:
+    if not path.exists(directory): mkdir(directory)
+
 def main(n: int):
   data = run_math(n)
 
@@ -33,6 +39,8 @@ def main(n: int):
   fig = px.line(chart_data, y="value", x="step")
   curr_time = time.localtime()
   
+  check_dirs()
+
   fig.write_image(f"./out_image/{curr_time.tm_year}.png")
   fig.write_html(f"./out_html/{curr_time.tm_year}.html")
 
