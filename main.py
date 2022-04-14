87cf836733c62920cd07b1d005b5fa77db9b0606
Lily <lolpo12345@outlook.com>
2022-04-14 03:38:26 +0300

    Finish logic

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import plotly.express as px
+import pandas as pd
 
 def main(n: int):
-  data = {}
+  data = []
   step = 0
 
   curr = n
   while curr != 1:
     if curr % 2 != 0:
-      data[step] = curr
-
+      data.append({"step": step, "value": curr})
       curr = (3 * curr) + 1
       step += 1
+      if curr == 1: 
+        print("end")
+        data.append({"step": step, "value": curr}) 
     else:
-      data[step] = curr
-
+      data.append({"step": step, "value": curr})
       curr = int(curr / 2)
       step += 1
+      if curr == 1: 
+        print("end")
+        data.append({"step": step, "value": curr}) 
+
+  
+  chart_data = pd.DataFrame(data)
+  
+  fig = px.line(chart_data, y="value", x="step")
+  
+  fig.show()
 
 main(27)
